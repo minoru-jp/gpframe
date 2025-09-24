ec621e2ef350d2b167cebe7e465bb9fcfdd6184e
minoru-jp <contactminoru@gmail.com>
2025-09-24 22:35:23 +0900

    Major breaking changes in v0.0.34

--- src/gpframe/impl/lifecycle/__init__.py
+++
@@ -1,8 +0,0 @@
-
-from .mod import Circuit
-
-from .phase import Phase, create_phase_manager_role, InvalidPhaseError
-from .phase import _Role as PhaseRole
-
-__all__ = ("Circuit",
-           "Phase", "create_phase_manager_role", "PhaseRole", "InvalidPhaseError")
