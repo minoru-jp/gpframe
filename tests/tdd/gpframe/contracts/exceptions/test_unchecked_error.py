2b6626e03e7ef2f1fdf2321d052b4252d5aae746
minoru-jp <contactminoru@gmail.com>
2025-10-17 19:33:33 +0900

    Delete

--- tests/tdd/gpframe/contracts/exceptions/test_unchecked_error.py
+++
@@ -1,7 +0,0 @@
-
-from gpframe.contracts.exceptions import UncheckedError
-
-def test_initial_creation_requires_frame_name_and_error():
-    err = UncheckedError("worker", ValueError("boom"))
-    assert err.frame_name == "worker"
-    assert isinstance(err.cause, ValueError)
